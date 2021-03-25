a524b29a78267ae8e913703d76ae036eb87be49f
MiDrAjX <jeisommarques78@gmail.com>
2021-03-25 10:42:57 -0300

    AppError/Rotas

--- Backend/src/routes/users.routes.ts
+++ Backend/src/routes/users.routes.ts
@@ -14,23 +14,21 @@ const upload = multer(uploadConfig);
 
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const { name, email, password } = request.body;
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const createUser = new CreateUserService();
 
-    delete user.password;
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    return response.json(user);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  delete user.password;
+
+  return response.json(user);
+
 });
 
 usersRouter.patch('/avatar',
@@ -38,20 +36,18 @@ usersRouter.patch('/avatar',
   upload.single('avatar'),
   async (request, response) => {
 
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
 
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-      delete user.password;
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    delete user.password;
+
+    return response.json(user);
+
 
   });
 
