00c8b4810209f91a747eb3914e7213ad20947c7e
MiDrAjX <jeisommarques78@gmail.com>
2021-04-05 12:32:48 -0300

    chore:

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -4,10 +4,16 @@ import SignIn from './pages/Signin/index';
 // import SignUp from './pages/SignUp/index';
 import GlobalStyle from './styles/global';
 
+import { AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+
     <GlobalStyle />
   </>
 );
+
 export default App;
